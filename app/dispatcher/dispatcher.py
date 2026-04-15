f160fc37a3d6fdc1f5d724ad1020310d7b785c28
Rodrigo Paulino <rodrigo.paulino@ibasolutions.com.br>
2026-04-14 22:39:21 -0300

    ajustando classes

--- app/dispatcher/dispatcher.py
+++ app/dispatcher/dispatcher.py
@@ -1,30 +1,21 @@
-# Tipagem forte
 from typing import List
-
-# Entidade de domínio
-from app.domain.work_item import WorkItem
-
-# Serviço de infraestrutura
 from app.services.excel_service import ExcelService
+from app.domain.work_item import WorkItem
 
 
 class Dispatcher:
 
-    # Recebe ExcelService por injeção
-    # Isso permite trocar implementação futuramente
-    def __init__(self, excel_service: ExcelService) -> None:
-        self._excel_service = excel_service
+    def __init__(self):
+        self._excel_service = ExcelService()
 
-    # Método principal do Dispatcher
-    # Responsável por "preparar" os itens
-    def dispatch(self, file_path: str) -> List[WorkItem]:
-
+    def run(self, file_path: str) -> List[WorkItem]:
         # Lê todos os itens do Excel
-        work_items = self._excel_service.read_work_items(file_path)
+        items = self._excel_service.read_work_items(file_path)
 
         # Filtra apenas itens válidos
-        # Regra de negócio começa aqui
-        valid_items = [item for item in work_items if item.is_valid()]
+        valid_items = [item for item in items if item.is_valid()]
 
-        # Retorna somente os válidos
+        print(f"Total lidos: {len(items)}")
+        print(f"Total válidos: {len(valid_items)}")
+
         return valid_items
--- app/main.py
+++ app/main.py
@@ -1,29 +1,15 @@
-# Importa dependências necessárias
-from app.services.excel_service import ExcelService
 from app.dispatcher.dispatcher import Dispatcher
 
 
-# Função principal do programa
-def main() -> None:
+def main():
+    dispatcher = Dispatcher()
 
-    # Instancia serviço de infraestrutura
-    excel_service = ExcelService()
-
-    # Injeta dependência no Dispatcher
-    dispatcher = Dispatcher(excel_service)
-
-    # Executa fluxo principal
-    items = dispatcher.dispatch(
+    items = dispatcher.run(
         r"C:\Users\rodri\Downloads\challenge.xlsx"
     )
 
-    # Exibe quantidade de itens válidos
-    print(f"Itens válidos: {len(items)}")
-
-    # Mostra primeiro item para debug
     print(items[0])
 
 
-# Garante execução apenas se rodar como módulo principal
 if __name__ == "__main__":
     main()
